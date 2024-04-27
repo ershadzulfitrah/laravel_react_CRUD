1d3a996680625f9325785f18bf89e5390af2c3c9
Ershad Zulfitrah Hans Dako <96755721+ershadzulfitrah@users.noreply.github.com>
2024-04-27 18:28:25 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Foundation\Application;
+
+use Illuminate\Support\Facades\Route;
+
+use Inertia\Inertia;
+
+use App\Http\Controllers\PostController;
+
+
+
+Route::resource('posts', PostController::class);
+
+
+
+Route::get('/', function () {
+
+    return Inertia::render('Welcome', [
+
+        'canLogin' => Route::has('login'),
+
+        'canRegister' => Route::has('register'),
+
+        'laravelVersion' => Application::VERSION,
+
+        'phpVersion' => PHP_VERSION,
+
+    ]);
+
+});
+
+
+
+Route::get('/dashboard', function () {
+
+    return Inertia::render('Dashboard');
+
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+require __DIR__.'/auth.php';
